49b71d0ab36830e2e29a2f3ed04cd67a75c13d34
Rohit Kumar <rohit@codebrewinnovations.com>
2020-01-24 18:41:31 +0530

    Initial Commit

---
+++ SwiftUI_Practices/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Practices
+//
+//  Created by Apple on 24/01/20.
+//  Copyright © 2020 Code Brew Labs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
