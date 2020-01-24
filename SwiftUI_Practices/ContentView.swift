b1fde4ff28e6bec995faedeafcedab4988b831b1
Rohit Kumar <rohit@codebrewinnovations.com>
2020-01-24 18:42:45 +0530

    Intial_Commit

--- SwiftUI_Practices/ContentView.swift
+++ SwiftUI_Practices/ContentView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        HStack {
+            Text("Hello World")
+            Text("Rohit Kumar")
+        }
+        
     }
 }
 
