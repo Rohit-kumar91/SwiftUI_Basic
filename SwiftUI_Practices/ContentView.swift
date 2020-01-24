0beed27c519ceafed6284e51fb063b356cecc976
Rohit Kumar <rohit@codebrewinnovations.com>
2020-01-24 19:20:43 +0530

    List IntialView

--- SwiftUI_Practices/ContentView.swift
+++ SwiftUI_Practices/ContentView.swift
@@ -9,12 +9,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+
+    
     var body: some View {
-        HStack {
-            Text("Hello World")
-            Text("Rohit Kumar")
+        NavigationView {
+            List {
+                ForEach(menu) { section in
+                    Text(section.name)
+                    
+                    ForEach(section.items) { item in
+                        Text(item.name)
+                    }
+                    
+                }
+            }.navigationBarTitle("Recipe")
         }
-        
     }
 }
 
